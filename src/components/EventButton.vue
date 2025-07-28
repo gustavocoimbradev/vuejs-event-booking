c531ab60a15150cd9a4b633c5517d620f93ed3c7
Gustavo Lima <whoisgustavolima@gmail.com>
2025-07-28 18:24:54 -0300

    event-booking

--- src/components/EventButton.vue
+++ src/components/EventButton.vue
@@ -23,8 +23,8 @@
                 'opacity-70 pointer-events-none': doing
             }"
     >
-        <div v-if="doing" class="flex items-center gap-1 justify-center">
-            <svg class="mr-3 size-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
+        <div v-if="doing" class="flex items-center gap-3 justify-center">
+            <svg class="size-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
             <span>{{ registered ? 'Canceling' : 'Registering' }}</span>
         </div>
         <div v-else>
