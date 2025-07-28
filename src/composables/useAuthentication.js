570c8209e885af4e6b5fb64f4827cdc0faa5f16c
Gustavo Lima <whoisgustavolima@gmail.com>
2025-07-28 18:15:49 -0300

    event-booking

--- src/composables/useAuthentication.js
+++ src/composables/useAuthentication.js
@@ -1,8 +1,8 @@
 import { ref } from 'vue'
 
 export default function useAuthentication() {
-    const user = ref({
-        id: 1
-    })
-    return { user }
+  const id = localStorage.getItem('user_id') || crypto.randomUUID()
+  localStorage.setItem('user_id', id)
+  const user = ref({ id })
+  return { user }
 }
