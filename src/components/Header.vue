72f2ecea2ce2c49c5b0e534ce2a225ab634ef569
Gustavo Lima <whoisgustavolima@gmail.com>
2025-07-28 18:27:02 -0300

    event-booking

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,6 @@
 
 <template>
     <div class="text-4xl text-white bg-indigo-500 w-[100dvw] mb-12 py-8 shadow-2xl shadow-slate-200 border-t-32 border-indigo-900">
-        <Container>Event Booking App</Container>
+        <Container>Event Booking</Container>
     </div>
 </template>
